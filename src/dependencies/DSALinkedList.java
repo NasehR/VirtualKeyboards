1726bc4c5f42303c9090cb331f101380fe4462d0
Naseh <75029758+NasehR@users.noreply.github.com>
2022-09-24 21:10:32 +0800

    Copied the necessary data structure files.

---
+++ src/dependencies/DSALinkedList.java
@@ -0,0 +1,240 @@
+package dependencies;
+
+import java.io.*;
+import java.util.*;
+
+public class DSALinkedList<T> implements Serializable, Iterable<T>
+{
+    // Class fields:
+    private int counter;
+    private DSAListNode head;
+    private DSAListNode tail;
+
+    // Linked List Node:
+    private class DSAListNode implements Serializable
+    {
+        // Class Fields:
+        private T data;
+        private DSAListNode prev;
+        private DSAListNode next;
+        
+        //Constructor:
+        public DSAListNode(T data)
+        {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    // Linked List Iterator:
+    private class DSALinkedListIterator implements Iterator<T>
+    {
+        // Class Fields:
+        private DSAListNode nextNode;
+        private DSAListNode current;
+
+        // Alternate Constructor:
+        public DSALinkedListIterator(DSALinkedList<T> list)
+        {
+            nextNode = list.head;
+            current = null;
+        }
+
+        //Getter hasNext:
+        public boolean hasNext()
+        {
+            return (nextNode != null);
+        }
+
+        //Setter next
+        public T next()
+        {
+            T value;
+            
+            if(nextNode == null)
+            {
+                value = null;
+            }
+
+            else
+            {
+                value = nextNode.data;
+                current = nextNode;
+                nextNode = nextNode.next;
+            }
+
+            return value;
+        }
+        
+        public void remove() throws IllegalStateException
+        {
+            throw new UnsupportedOperationException("Unsupported Operation.");
+        }
+    }
+
+    //Constructor:
+    public DSALinkedList() 
+    {
+        head = null;
+        tail = null;
+        counter = 0;
+    }
+
+    //Getter iterator:
+    public Iterator<T> iterator()
+    {
+        return new DSALinkedListIterator(this);
+    }
+
+    //Setter insertFirst:
+    public void insertFirst(T newValue)
+    {
+        DSAListNode newNode = new DSAListNode(newValue);
+
+        if(isEmpty())
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        
+        else
+        {
+            newNode.next = head;
+            head.prev = newNode;
+        }
+
+        head = newNode;
+        counter++;
+    }
+
+    //Setter insertLast:
+    public void insertLast(T newValue)
+    {
+        DSAListNode newNode = new DSAListNode(newValue) ;
+
+        if(isEmpty())
+        {
+            head = newNode;
+            tail = newNode;
+        }
+
+        else
+        {
+            newNode.prev = tail;
+            tail.next = newNode;
+
+            tail = newNode;
+        }
+
+        tail = newNode;
+        counter++;
+    }
+
+    //Setter removeFirst:
+    public T removeFirst() throws NoSuchElementException
+    {    
+        T data;
+        DSAListNode tmp;
+
+        if(isEmpty())
+        {
+            throw new NoSuchElementException("List is empty");
+        }
+
+        data = head.data;
+
+        if(counter == 1)
+        {
+            head = null;
+            tail = null;
+        }
+        else
+        {
+            head = head.next;
+
+            head.prev = null;
+        }
+
+        counter--;
+
+        return data;
+    }
+
+    //Setter removeLast:
+    public T removeLast() throws NoSuchElementException
+    {
+        T data;
+        DSAListNode tmp;
+
+        if(isEmpty())
+        {
+            throw new NoSuchElementException("List is empty");
+        }
+
+        data = tail.data;
+
+        if(counter == 1)
+        {
+            head = null;
+            tail = null;
+        }
+        else
+        {
+            tail = tail.prev;
+
+            tail.next = null;
+        }
+
+        counter--;
+        
+        return data;
+    }
+
+    //Getter isEmpty:
+    public boolean isEmpty()
+    {
+        return ((head == null) && (tail == null));
+    }
+
+    //Getter getcounter:
+    public int getCount()
+    {
+        return counter;
+    }
+
+    //Getter peekFirst:
+    public T peekFirst()
+    {
+        if(isEmpty())
+        {
+            throw new NoSuchElementException("List is empty");
+        }
+
+        return (head.data);
+    }
+
+    //Getter peekLast:
+    public T peekLast()
+    {
+        if(isEmpty())
+        {
+            throw new NoSuchElementException("List is empty");
+        }
+
+        return (tail.data);
+    }
+
+    public void display()
+    {
+        DSAListNode tmp;
+
+        tmp = head;
+
+        while(tmp != null)
+        {
+            System.out.println(tmp.data);
+            tmp = tmp.next;
+        }
+    }
+}
---
+++ src/dependencies/DSAQueue.java
@@ -0,0 +1,86 @@
+package dependencies;
+
+import java.util.*;
+
+public class DSAQueue<T> implements Iterable<T>
+{
+    //Class fields
+    private DSALinkedList<T> queue;
+    private int capacity;
+
+    //Constructor
+    public DSAQueue()
+    {
+        queue  = new DSALinkedList<T>();
+        capacity = 100;
+    }
+
+    //Alternate Constructor
+    public DSAQueue(int capacity) throws IllegalArgumentException
+    {
+        if(capacity < 1)
+        {
+            throw new IllegalArgumentException("Capacity must be greater than 1");
+        }
+
+        queue = new DSALinkedList<T>();
+        this.capacity = capacity;
+    }
+
+    //Getter Iterator:
+    public Iterator<T> iterator()
+    {
+        return queue.iterator();
+    }
+    
+    //Getter Capacity:
+    public int getCount()
+    {
+        return (queue.getCount());
+    }
+
+    //Getter isEmpty:
+    public boolean isEmpty()
+    {
+        return (queue.isEmpty());
+    }
+
+    //Getter isFull:
+    public boolean isFull()
+    {
+        return (queue.getCount() == capacity);
+    }
+
+    //Setter enqueue:
+    public void enqueue(T value) throws IllegalStateException
+    {
+        if(isFull())
+        {
+            throw new IllegalStateException("The queue is full.");
+        }
+
+        queue.insertLast(value);
+    }
+
+    //Setter dequeue:
+    public T dequeue() throws NoSuchElementException
+    {
+        if(isEmpty())
+        {
+            throw new NoSuchElementException("The queue is empty.");
+        }
+
+        return (queue.removeFirst());
+    }
+
+    //Getter peek:
+    public T peek() throws NoSuchElementException
+    {
+        if(isEmpty())
+        {
+            throw new NoSuchElementException("The queue is empty.");
+        }
+        
+        return (queue.peekFirst());
+    }
+}
